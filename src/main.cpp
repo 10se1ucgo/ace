e20540e9648df350c69e267c17957494525fb107
10se1ucgo <10se1ucgo@users.noreply.github.com>
2018-09-14 21:14:17 -0400

    Make pybind11 optional for now.

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,7 @@
 #include <iostream>
 
 #include "game_client.h"
-#include "scene/loading.h"
 #include "scene/menu.h"
-#include "scene/block_test.h"
 
 int main(int argc, char **argv) {
 #ifdef NDEBUG
@@ -13,7 +11,7 @@ int main(int argc, char **argv) {
 #endif
         ace::GameClient client("ACE: \"Ace of Spades\" CliEnt");
         std::string ip(argc > 1 ? argv[1] : "aos://180274501:32887:0.75");
-        client.set_scene<ace::scene::MapEditor>();
+        client.set_scene<ace::scene::MainMenuScene>();
         client.run();
         return 0;
 #ifdef NDEBUG
